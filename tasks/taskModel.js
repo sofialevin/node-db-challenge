b1a04088456dbf013a4a046577c5fb9c445cf9a7
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-13 09:31:21 -0800

    Completed taskModel.

--- tasks/taskModel.js
+++ tasks/taskModel.js
@@ -1,1 +1,19 @@
 const db = require('../data/db-config.js');
+
+function find() {
+  return db('tasks');
+}
+
+function add(task) {
+  db('tasks')
+    .insert(task)
+    .then(res => {
+      const id = res[0];
+      return db("tasks").where({ id });
+    });
+}
+
+module.exports = {
+  find,
+  add
+};
