12f59e33e5aead10212a01b4f93e6701c7c7dde6
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-13 09:54:12 -0800

    Completed get endpoint on resourceRouter.

--- resources/resourceRouter.js
+++ resources/resourceRouter.js
@@ -4,4 +4,10 @@ const Resources = require('./resourceModel.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Resources.find()
+  .then(resources => res.status(200).json(resources))
+  .catch(err => res.status(500).json({ message: "'Failed to get resources from database'", error: err }))
+})
+
 module.exports = router;
