38ab9b5484cc598c9e3121d5000c4f795cbf6cb9
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-13 09:29:45 -0800

    Completed projectModel.

--- projects/projectModel.js
+++ projects/projectModel.js
@@ -1,1 +1,19 @@
 const db = require('../data/db-config.js');
+
+function find() {
+  return db('projects');
+}
+
+function add(project) {
+  db('projects')
+    .insert(project)
+    .then(res => {
+      const id = res[0];
+      return db("projects").where({ id });
+    });
+}
+
+module.exports = {
+  find,
+  add
+};
