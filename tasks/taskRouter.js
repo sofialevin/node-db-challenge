c2895ca9b74dddfbaa7b3402928b4b1b08945375
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-13 10:31:34 -0800

    Completed get postendpoint on taskRouter.

--- tasks/taskRouter.js
+++ tasks/taskRouter.js
@@ -10,4 +10,24 @@ router.get('/', (req, res) => {
   .catch(err => res.status(500).json({ message: "'Failed to get tasks from database'", error: err }))
 })
 
+router.post('/', checkData, (req, res) => {
+  const { body } = req
+  
+  Tasks.add(body)
+    .then(task => res.status(201).json({...task, completed: task.completed ? true : false}))
+    .catch(err => res.status(500).json({ message: "'Failed to create task in database'", error: err }))
+})
+
+function checkData(req, res, next) {
+  const { body } = req
+
+  if (!body.project_id) {
+    res.status(400).json({ message: "Please provide a project_id for the task."})
+  } else if (!body.task_description) {
+    res.status(400).json({ message: "Please provide a description for the task."})
+  } else {
+    next()
+  }
+}
+
 module.exports = router;
